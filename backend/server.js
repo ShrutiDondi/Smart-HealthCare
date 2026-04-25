3f58e59a933563ff2966233ff18fdca4ebc45d29
ShrutiDondi <dhondishruti@gmail.com>
2026-04-25 11:40:52 +0530

    Ready for deployment

--- backend/server.js
+++ backend/server.js
@@ -45,3 +45,10 @@ async function startServer() {
 }
 
 startServer();
+const path = require("path");
+
+app.use(express.static(path.join(__dirname, "../client/build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
